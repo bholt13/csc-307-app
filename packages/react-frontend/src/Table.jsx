d05349ba65b2c5409cae31a3f3136136b6c47f69
bholt13 <105313389+bholt13@users.noreply.github.com>
2024-04-11 23:06:49 -0700

    Update Table.jsx

--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -28,15 +28,13 @@ function TableBody(props) {
 }
 function Table(props) {
     return (
-    <tr key={index}>
-      <td>{row.name}</td>
-      <td>{row.job}</td>
-      <td>
-        <button onClick={() => props.removeCharacter(index)}>
-          Delete
-        </button>
-      </td>
-    </tr>
+    <table>
+      <TableHeader />
+      <TableBody
+        characterData={props.characterData}
+        removeCharacter={props.removeCharacter}
+      />
+    </table>
   );
 }
 export default Table;
