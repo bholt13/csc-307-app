213d8898c1163ff416aacfa824678d74a284e715
bholt13 <105313389+bholt13@users.noreply.github.com>
2024-04-11 21:51:10 -0700

    Update Myapp.jsx

--- packages/react-frontend/src/Myapp.jsx
+++ packages/react-frontend/src/Myapp.jsx
@@ -1,27 +1,25 @@
 // src/MyApp.jsx
-import React from "react";
+import React, { useState } from "react";
 import Table from "./Table";
 
-const characters = [
-  {
-    name: "Charlie",
-    job: "Janitor"
-  },
-  {
-    name: "Mac",
-    job: "Bouncer"
-  },
-  {
-    name: "Dee",
-    job: "Aspring actress"
-  },
-  {
-    name: "Dennis",
-    job: "Bartender"
-  }
-];
-
 function MyApp() {
+    const [characters, setCharacters] = useState([
+    {
+      name: "Charlie",
+      job: "Janitor"
+    },
+    {
+      name: "Mac",
+      job: "Bouncer"
+    },
+    {
+      name: "Dee",
+      job: "Aspring actress"
+    },
+    {
+      name: "Dennis",
+      job: "Bartender"
+  }
   return (
   <div className="container">
     <Table characterData={characters} />
