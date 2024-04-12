1ed97e5c9dc4cec747a4638284fd1d5038bdbe17
bholt13 <105313389+bholt13@users.noreply.github.com>
2024-04-11 21:53:22 -0700

    Update Myapp.jsx

--- packages/react-frontend/src/Myapp.jsx
+++ packages/react-frontend/src/Myapp.jsx
@@ -19,12 +19,24 @@ function MyApp() {
     {
       name: "Dennis",
       job: "Bartender"
+    }
+  ]);
+    
+   function removeOneCharacter(index) {
+       const updated = characters.filter((character, i) => {
+      return i !== index;
+    });
+    setCharacters(updated);
   }
   return (
   <div className="container">
-    <Table characterData={characters} />
+    <Table
+      characterData={characters}
+      removeCharacter={removeOneCharacter}
+    />
   </div>
-  );
+);
+    
 }
 
 export default MyApp;
