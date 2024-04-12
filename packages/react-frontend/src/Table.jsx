8ebe5ff4b7934b774b5715bfcfaa2e2385756a65
bholt13 <105313389+bholt13@users.noreply.github.com>
2024-04-11 21:45:57 -0700

    Update Table.jsx

--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -1,35 +1,34 @@
 // src/Table.jsx
-import React from "react";
-
-function Table() {
+function TableHeader() {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Job</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Charlie</td>
-          <td>Janitor</td>
-        </tr>
-        <tr>
-          <td>Mac</td>
-          <td>Bouncer</td>
-        </tr>
-        <tr>
-          <td>Dee</td>
-          <td>Aspiring actress</td>
-        </tr>
-        <tr>
-          <td>Dennis</td>
-          <td>Bartender</td>
-        </tr>
-      </tbody>
-    </table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Job</th>
+      </tr>
+    </thead>
   );
 }
 
-export default Table;
+function TableBody() {
+  return (
+    <tbody>
+      <tr>
+        <td>Charlie</td>
+        <td>Janitor</td>
+      </tr>
+      <tr>
+        <td>Mac</td>
+        <td>Bouncer</td>
+      </tr>
+      <tr>
+        <td>Dee</td>
+        <td>Aspiring actress</td>
+      </tr>
+      <tr>
+        <td>Dennis</td>
+        <td>Bartender</td>
+      </tr>
+    </tbody>
+  );
+}
