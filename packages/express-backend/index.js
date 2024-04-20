15bfb3e07b0a6741b9f192fd2b82fe8cb70d90cc
Brendan Holt <holt0316@gmail.com>
2024-04-19 22:34:38 -0700

    step 7 done

--- packages/express-backend/index.js
+++ packages/express-backend/index.js
@@ -46,14 +46,68 @@ app.post("/users", (req, res) => {
   res.send();
 });
 
+const deleteUserById = (userId) => {
+  const userToDelete = findUserById(userId);
+  if (!userToDelete) {
+    return null;
+  }
+  const index = users["users_list"].indexOf(userToDelete);
+  users["users_list"].splice(index, 1);
+  return true;
+};
+
+app.delete("/users/:id", (req, res) => {
+  const userId = req.params.id;
+  console.log(userId);
+  const deletedUser = deleteUserById(userId);
+  if (!deletedUser) {
+    res.status(404).send("User not found");
+  } else {
+    res.send("User deleted successfully");
+  }
+});
+
+const findUserByName = (name) => {
+  return users["users_list"].filter(
+    (user) => user["name"] === name
+  );
+};
+
+app.get("/users", (req, res) => {
+  const name = req.query.name;
+  const job = req.query.job;
+  
+  if (name && job) {
+    const matchedUsers = findUsersByNameAndJob(name, job);
+    if (matchedUsers.length === 0) {
+      return res.status(404).send("User not found");
+    }
+    res.send({ users_list: matchedUsers });
+    } else if (name) {
+    	const matchedUsers = findUserByName(name);
+    	if (matchedUsers.length === 0) {
+      	return res.status(404).send("User not found");
+    }
+    res.send({ users_list: matchedUsers });
+  } else {
+    res.send(users);
+  }
+});
+
+
 const findUserById = (id) =>
   users["users_list"].find((user) => user["id"] === id);
+
+const findUsersByNameAndJob = (name, job) =>
+  users["users_list"].filter((user) => {
+    return user["name"].toLowerCase() === name.toLowerCase() && user["job"].toLowerCase() === job.toLowerCase();
+  });
 
 app.get("/users/:id", (req, res) => {
   const id = req.params["id"]; //or req.params.id
   let result = findUserById(id);
   if (result === undefined) {
-    res.status(404).send("Resource not found.");
+    res.status(404).send("User not found");
   } else {
     res.send(result);
   }
